58bdc1623b3d9aefc59aaf596c991e1f03898080
Snowball0409 <kai890409@gmail.com>
2022-05-21 15:45:31 +0800

    Simple Softmax Class done

--- softmax.py
+++ softmax.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 class softmax:
-    def __init__():
-        print("do something")
+    def __init__(self):
+        print("Create Softmax...")
     
-    def __call__():
-        print("do something")
+    def __call__(self,inputs):
+        return np.exp(inputs)/sum(np.exp(inputs))
