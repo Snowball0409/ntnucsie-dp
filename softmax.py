02eefdec451ac4b78acc86bbf632d2122d8c59e8
Snowball0409 <kai890409@gmail.com>
2022-06-17 21:12:42 +0800

    Simple softmax loss complete

--- softmax.py
+++ softmax.py
@@ -1,8 +1,10 @@
 import numpy as np
+import tensorflow as tf
 
 class softmax:
     def __init__(self):
         print("Create Softmax...")
     
-    def __call__(self,inputs):
-        return np.exp(inputs)/sum(np.exp(inputs))
+    def Softmax(self,y_pred,y_true):
+        L = tf.reduce_sum(y_true-np.log(np.exp(y_pred)/sum(np.exp(y_pred))))
+        return 1/(y_true.size)*L
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import numpy as np
 def main():
     loss = softmax()
     input = np.array([1,2,3,4,5])
-    print(loss(input))
+    print(loss.Softmax(input,input))
+    print(logcosh_loss(input,input))
 
 main()
