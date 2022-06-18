a4e2ad2d6736c9f0a73cd1c75b481c62f564f6f3
Snowball0409 <kai890409@gmail.com>
2022-06-18 16:52:46 +0800

    Solve dtype error.

--- softmax.py
+++ softmax.py
@@ -7,7 +7,7 @@ class softmax:
     
     def Softmax(self,y_true,y_pred):
         L = tf.reduce_sum(y_true-np.log(np.exp(y_pred)/sum(np.exp(y_pred))))
-        return 1/(y_true.size)*L
+        return 1/(y_true.size)*tf.cast(L,dtype=tf.float32)
     
     # def L_Softmax(self,y_true,y_pred):
     #     L = 
